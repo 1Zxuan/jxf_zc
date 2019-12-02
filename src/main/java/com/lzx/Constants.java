45a52105d08763e918f8ca129b920d32761c5ec8
楼中煊 <64390357@qq.com>
2019-12-02 14:27:25 +0800

    新增常量配置

---
+++ src/main/java/com/lzx/Constants.java
@@ -0,0 +1,25 @@
+package com.lzx;
+
+/**
+ * @author 1Zx.
+ * @data 2019/12/2 10:27
+ */
+public interface Constants {
+
+    public static final Integer version = 1;
+
+    public static final String ConfigURL = "config.url";
+
+    public static final String AppStatic = "app.static";
+
+    public static final String AppVersion = "app.version";
+
+    public static final String COMMAND = "app.command";
+
+    public static final String ZCFileName = "app.zc";
+
+    public static final String CGFileName = "app.cg";
+
+    public static final String ResultSize = "result.size";
+
+}
