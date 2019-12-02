e70b6cca4a89af0a0a784ba3b683d69a00af9984
楼中煊 <64390357@qq.com>
2019-12-02 10:19:12 +0800

    初始化提交

---
+++ src/main/java/com/lzx/Main.java
@@ -0,0 +1,13 @@
+package com.lzx;
+
+/**
+ * @author 1Zx.
+ * @data 2019/12/2 9:59
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String path = args[0];
+
+    }
+}
