6d71e540696e9aa9ad6ff0ae84341e74e8fd9b39
楼中煊 <64390357@qq.com>
2019-12-02 16:31:23 +0800

    完善代码

--- src/main/java/com/lzx/Constants.java
+++ src/main/java/com/lzx/Constants.java
@@ -8,7 +8,7 @@ public interface Constants {
 
     public static final Integer version = 1;
 
-    public static final String ConfigURL = "config.url";
+    public static final String ConfigURL = "https://raw.githubusercontent.com/1Zxuan/jxf_zc/master/src/main/resources/config.properties";
 
     public static final String AppStatic = "app.static";
 
@@ -21,5 +21,31 @@ public interface Constants {
     public static final String CGFileName = "app.cg";
 
     public static final String ResultSize = "result.size";
+
+    //标记目标邮件
+    public static final String EMAILRECEIVEURL = "receive.email.url";
+
+    public static final String EMAILFROMURL = "from.email.url";
+
+    //邮箱类型
+    public static final String EMAILTYPE = "email.type";
+
+    //邮箱连接协议
+    public static final String EMAILPROTOCOL = "email.protocol";
+
+    public static final String EMAILAUTH = "email.auth";
+
+    //邮箱主机名
+    public static final String EMAILHOST = "email.host";
+
+    //邮箱端口号
+    public static final String EMAILPORT = "email.port";
+
+    public static final String AuthorizationCode = "email.authorizationCode";
+
+    public static final String EMAILSSLENABLE = "email.smtp.ssl.enable";
+
+    public static final String EMAILDEBUG="email.debug";
+
 
 }
